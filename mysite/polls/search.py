f83982a12153cfa48dddcb01e9f58bf8bfd8e340
skthinks <sushcool94@gmail.com>
2016-06-07 19:24:24 +0530

    Making some recommended changes

--- mysite/polls/search.py
+++ mysite/polls/search.py
@@ -1,21 +1,22 @@
-import requests
 import re
+
+import requests
 from more_itertools import unique_everseen
 
+import imdb_url
 
 def get_possible_movie_ids(movie_name):
-    url = "http://www.imdb.com/find?ref_=nv_sr_fn&q="+movie_name+"&s=all"
+    url = "%s%s&s=all" %(imdb_url.id_url, movie_name)
     r = requests.get(url)
     movie_id = re.findall(r'tt[0-9]+', r.text, re.M)
     if not movie_id:
-        print "No movie matches this description"
-        exit()
+        return []
     movie_id_list = list(unique_everseen(movie_id))
     return movie_id_list
 
 
 def get_name_of_movie(movie_id):
-    url = "http://imdb.com/title/"+movie_id+"/?ref_=fn_al_tt_1"
+    url = "%s%s/?ref_=fn_al_tt_1" %(imdb_url.name_url, movie_id)
     r = requests.get(url)
     movie_name = re.search(r'<title>.*IMDb', r.text)
     return movie_name.group()
@@ -23,60 +24,85 @@ def get_name_of_movie(movie_id):
 
 def get_movie_names(movie_name):
     movie_id_list = get_possible_movie_ids(movie_name)
+    if movie_id_list == []:
+        return [],[]
     movie_name_list = []
-    id_name_map = {}
     for movie_id in movie_id_list:
         movie_name = get_name_of_movie(movie_id)
         movie_name = movie_name.replace("<title>", "").replace("- IMDb", "")
         movie_name_list.append(movie_name) 
-        print movie_name
-    return movie_id_list, movie_name_list, id_name_map 
+    return movie_id_list, movie_name_list 
 
 
 def get_title(r_text):
-    movie_name = re.search(r'<title>.*IMDb', r_text)
-    movie_name = movie_name.group()
-    movie_name = movie_name.replace("<title>", "").replace("- IMDb", "")
+    try:
+        movie_name = re.search(r'<title>.*IMDb', r_text)
+        movie_name = movie_name.group()
+        movie_name = movie_name.replace("<title>", "").replace("- IMDb", "")
+    except AttributeError:
+        movie_name = "N/A"
     return movie_name
 
 
 def get_rating(r_text):
-    rating = re.search('ratingValue......', r_text).group()
-    rating = re.search(r'[0-9]+.[0-9]+', rating).group()
+    try:
+        rating = re.search('ratingValue......', r_text).group()
+        rating = re.search(r'[0-9]+.[0-9]+', rating).group()
+    except AttributeError:
+        rating = "N/A"
     return rating
 
 
 def get_year(r_text):
-    year = re.search('titleYear.*', r_text).group()
-    year = re.search('[0-9]+', year).group()
+    try:
+        year = re.search('titleYear.*', r_text).group()
+        year = re.search('[0-9]+', year).group()
+    except AttributeError:
+        year = "N/A"
     return year
 
 
 def get_directors(r_text):
-    directors = re.search('Directed by .*', r_text).group()
-    directors = directors.split('With ')
+    directors = []
+    directors.append("N/A")
+    try:
+        directors = re.search('Directed by .*', r_text).group()
+        directors = directors.split('With ')
+    except:
+        directors[0] = "N/A"
     return directors[0]
 
 
 def get_actors(r_text):
-    directors = re.search('Directed by .*', r_text).group()  
-    directors = directors.split('With ')
-    actors = re.match(r'(?:[^.:;]+[.:;]){1}', directors[1]).group() 
+    directors = []
+    directors.append("N/A")
+    try:
+        directors = re.search('Directed by .*', r_text).group()  
+        directors = directors.split('With ')
+        actors = re.match(r'(?:[^.:;]+[.:;]){1}', directors[1]).group() 
+    except:
+        actors = "N/A"
     return actors
 
 
 def get_duration(r_text):
-    duration = re.search('[0-9]h [0-9]+min', r_text).group()
+    try:
+        duration = re.search('[0-9]h [0-9]+min', r_text).group()
+    except:
+        duration = "N/A"
     return duration
 
 
 def get_genres(r_text):
-    genre = re.findall('genre/[A-Z].*inf"', r_text)
-    genres = []
-    for gen in genre:
-        gen = re.search('/.*\?', gen).group()
-        gen = gen.replace('/', "").replace('?', "")
-        genres.append(gen)
+    try:
+        genre = re.findall('genre/[A-Z].*inf"', r_text)
+        genres = []
+        for gen in genre:
+            gen = re.search('/.*\?', gen).group()
+            gen = gen.replace('/', "").replace('?', "")
+            genres.append(gen)
+    except:
+        genres = []
     return genres
 
 
