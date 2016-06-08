bbe0b71a38dd175c7e61fb5c0f5c0b7375f1c952
skthinks <sushcool94@gmail.com>
2016-06-08 11:07:20 +0530

    Attempt

--- polls/imdb_url.py
+++ polls/imdb_url.py
@@ -1,4 +1,4 @@
 id_url = "http://www.imdb.com/find?ref_=nv_sr_fn&q="
 name_url = "http://imdb.com/title/" 
-query_url = "http://localhost:8000/polls/info/?q="
-search_url = "http://localhost:8000/polls/suggest/?q="
+query_url = "https://glacial-garden-97175.herokuapp.com/"
+search_url = "https://glacial-garden-97175.herokuapp.com/"
