9988ffd4a88d292527c6ea3065462e7d12d54dbe
skthinks <sushcool94@gmail.com>
2016-06-09 16:47:41 +0530

    Attempt

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
-    url(r'', views.display_welcome, name='First Page'),
+urlpatterns = [`
     url(r'info/.*', views.display_info, name='information'),
     url(r'suggest/.*', views.index, name='search'),
+    url(r'', views.display_welcome, name='First Page'),
 ]
